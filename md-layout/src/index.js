fd37944547655b57c9ea2f5e434e3da91dc6715e
Mihai <mveres@users.noreply.github.com>
2021-05-04 12:17:27 +0300

    colors and typography tokens and stories for them (#2)

--- md-layout/src/index.js
+++ md-layout/src/index.js
@@ -2,6 +2,7 @@ export { html } from 'lit-html';
 import '@shoelace-style/shoelace/dist/themes/base.css';
 import '@shoelace-style/shoelace/dist/shoelace.js';
 import { registerIconLibrary } from '@shoelace-style/shoelace/dist/utilities/icon-library.js';
+import { themeStyle } from '~/theme';
 
 registerIconLibrary('default', {
   resolver: (name) =>
@@ -9,3 +10,11 @@ registerIconLibrary('default', {
 });
 
 export { registerIconLibrary };
+
+const setTheme = () => {
+  const body = document.querySelector('body');
+  body.setAttribute('class', 'sl-theme-ds');
+  body.innerHTML = `${themeStyle}${body.innerHTML}`;
+};
+
+setTheme();
